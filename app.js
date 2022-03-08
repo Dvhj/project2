0650276d260300ae613f9fe27d81b64efc510f0f
Dvhj <kirillov97@list.ru>
2022-03-09 00:20:17 +0400

    two

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const carts = document.querySelectorAll('.cart')
+const winCont = document.querySelector('.winContainer')
 
-console.log(carts)
-
+let array = []
 let count = 0;
 let x;
 let y;
@@ -9,14 +9,14 @@ let falseCount = 100;
 
 carts.forEach(cart => {
 	cart.addEventListener('click', () => {
-		if ( count == 0 && cart.id < 100) {
+		if ( count === 0 && cart.id < 100) {
 			count++
 			cart.children[0].style.display = 'block'
 			x = cart.id
 			localStorage.setItem(`${cart.id}`, cart.dataset.text)
-		} else if (cart.id == x || cart.id > 100) {
+		} else if (cart.id === x || cart.id > 100) {
 			return
-		} else if (count == 1) {
+		} else if (count === 1) {
 			count = 0
 			cart.children[0].style.display = 'block'
 			y = cart.id
@@ -29,15 +29,37 @@ carts.forEach(cart => {
 })
 
 function comeback(cart){
-	 if (localStorage.getItem(x) == localStorage.getItem(y)) {
+	if (localStorage.getItem(x) === localStorage.getItem(y)) {
 	 	console.log(cart.id = `${++falseCount}`)
 	 	console.log(carts[x-1].id = `${++falseCount}`)
 	 	localStorage.clear()
-	 } else {
+	} else {
 	 	setTimeout( () => {carts[x-1].children[0].style.display = 'none'
 	 	carts[y-1].children[0].style.display = 'none'
 		localStorage.clear()}, 500)
-	 	
-	 }
-	
+	}
+	win()
 }
+
+const win = () => {
+	carts.forEach( item => {
+		if (item.id < 100) {
+			return
+		} else {
+			if (array.includes(item.id)){
+				return
+			} else {
+				array.push(item.id)
+				console.log(array)
+				if (array.length == 16) {
+					console.log('win!!')
+					let winBox = document.createElement('div')
+					winBox.classList.add('winBox')
+					winBox.innerHTML = 'ПОБЕДА'
+					winCont.append(winBox)
+				}
+			}
+		}
+	})
+}
+
